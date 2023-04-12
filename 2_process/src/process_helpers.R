202be88f9294170069310930981944b71fc2a06d
Lindsay Platt <lrplatt@wisc.edu>
2023-04-12 15:32:13 -0500

    adjust raster process output format & add summary target for counts of classes per mission/year

--- 2_process/src/process_helpers.R
+++ 2_process/src/process_helpers.R
@@ -6,12 +6,29 @@ stacked_netcdf_to_raster_list <- function(nc_file) {
   # For each date, extract the values and save as a list of rasters
   out_raster_list <- purrr::map(nc_atts, function(date) {
     var_atts <- ncatt_get(nc, date)
-    list_name <- sprintf('%s_%s', var_atts$mission, var_atts$date)
     list(
+      mission = var_atts$mission,
+      date = var_atts$date,
       raster_vals = raster(ncvar_get(nc, date))
-    ) %>% setNames(nm = list_name)
-  }) %>% unlist()
+    )
+  })
   
   nc_close(nc)
   return(out_raster_list)
+}
+
+summarize_raster_class_counts <- function(raster_list) {
+  
+  # Extract the raster object from the list
+  raster_vals <- raster_list$raster_vals
+  
+  # Add a class of 5, which will be the NAs
+  raster_vals[is.na(raster_vals)]<-5
+  
+  # Count the number of pixels in each class
+  raster::freq(raster_vals) %>% 
+    as_tibble() %>% 
+    mutate(mission = raster_list$mission,
+           date = raster_list$date) %>% 
+    select(mission, date, class = value, count)
 }
